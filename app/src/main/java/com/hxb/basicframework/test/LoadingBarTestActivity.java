e8b10e4f8f3ce9e94d9fef8a697c3204bd63f57c
cshxbz <cshxbz@163.com>
2019-07-26 15:32:54 +0800

    some commit

--- app/src/main/java/com/hxb/basicframework/test/LoadingBarTestActivity.java
+++ app/src/main/java/com/hxb/basicframework/test/LoadingBarTestActivity.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.FrameLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.dyhdyh.widget.loadingbar2.LoadingBar;
 import com.hxb.basicframework.R;
+
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,13 +22,14 @@ import butterknife.OnClick;
 public class LoadingBarTestActivity extends AppCompatActivity {
 
     @BindView(R.id.fl_content)
-    FrameLayout flContent;
+    ViewGroup vgContent;
 
     private Handler handler = new Handler();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_bar_test);
         ButterKnife.bind(this);
 
 
@@ -31,16 +38,67 @@ public class LoadingBarTestActivity extends AppCompatActivity {
 
     @OnClick({R.id.btn_show_loading_view})
     public void showLoadingView(){
-        LoadingBar.view(flContent).show();
-        handler.postDelayed(() -> {
-            LoadingBar.view(flContent).cancel();
+        showCustomLoadingView();
+    }
+
+
+    private ProgressBar pbLoading;
+    private TextView tvLoadingFail;
+
+    private void showCustomLoadingView(){
+        View view = LayoutInflater.from(this).inflate(R.layout.loading_view, null);
+
+        pbLoading = view.findViewById(R.id.pb_loading);
+        tvLoadingFail = view.findViewById(R.id.tv_fail);
+
+        pbLoading.setVisibility(View.VISIBLE);
+        tvLoadingFail.setVisibility(View.GONE);
+
+        tvLoadingFail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pbLoading.setVisibility(View.VISIBLE);
+                tvLoadingFail.setVisibility(View.GONE);
+                simulateIo();
+            }
+        });
+
+        LoadingBar.view(vgContent)
+                .setFactoryFromView(view)
+                .show();
+
+        simulateIo();
+    }
+
+
+    private void simulateIo(){
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Random r = new Random();
+                int i = r.nextInt(2);
+
+                if(i==1){
+                    LoadingBar.view(vgContent).cancel();
+                }else{
+                    pbLoading.setVisibility(View.GONE);
+                    tvLoadingFail.setVisibility(View.VISIBLE);
+                }
+            }
         }, 3000);
     }
 
 
     @OnClick({R.id.btn_show_loading_dialog})
     public void showLoadingDialog(){
-
+        LoadingBar.dialog(this).show();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                LoadingBar.dialog(LoadingBarTestActivity.this).cancel();
+            }
+        }, 3000);
     }
 
 }
